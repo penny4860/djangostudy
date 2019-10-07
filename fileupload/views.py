5909f38ab9159b04a5e5a5cdf85eb0dde5493c4a
jeongjoonsup <penny4860@gmail.com>
2019-10-07 20:35:41 +0900

    working on

--- fileupload/views.py
+++ fileupload/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponseRedirect
+from .forms import UploadFileForm
+
+# from somewhere import handle_uploaded_file
+
+# Imaginary function to handle an uploaded file.
+def handle_uploaded_file(f):
+    with open('tmp.txt', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
