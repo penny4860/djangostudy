4b3a24d51430931767b5c98656760b8f99f8057c
jeongjoonsup <penny4860@gmail.com>
2019-10-15 22:40:29 +0900

    update

--- djangostudy/urls.py
+++ djangostudy/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from fileupload import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('upload/', views.upload_file, name='upload'),
-    path('snippets/', include('snippets.urls')),
 ]
 
 
